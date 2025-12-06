441c6e869752df62777667c101666882c153338a
abdomrieden <abdo.mrieden@gmail.com>
2025-12-06 15:59:34 +0200

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,5 @@
+public class Customer extends Person{
+    public Customer(int id, String name, String phone) {
+        super(id,name,phone);
+    }
+}
---
+++ Person.java
@@ -0,0 +1,55 @@
+public abstract class Person{
+    protected int id;
+    protected String name;
+    protected String phone;
+
+    public Person(int id, String name, String phone){
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getPhone() {
+        return phone;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
