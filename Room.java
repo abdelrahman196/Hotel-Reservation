568d138850fc25824d7ac24b13a342bc915fac34
abdelrahman196 <abdelrahmanhelmy195@gmail.com>
2025-12-11 16:23:25 +0200

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,14 +1,25 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Room {
     private int id;
+    private ArrayList<Customer> residents;
+    private ArrayList<Integer> services;
     private String type;
-    private double price;
-    private boolean isOccupied;
+    private int price;
+    private boolean available;
+    private Date entryDate;
+    private Date expirationDate;
 
-    public Room(int id, String type, double price, boolean isOccupied) {
+    public Room(int id, String type, int price, boolean isOccupied) {
         this.id = id;
         this.type = type;
         this.price = price;
-        this.isOccupied = isOccupied;
+        this.available = available;
+        this.residents = new ArrayList<>();
+        this.services = new ArrayList<>();
+        this.entryDate = new Date();
+        this.expirationDate = new Date();
     }
 
     public int getId() {
@@ -17,13 +28,38 @@ public class Room {
     public String getType() {
         return type;
     }
-    public double getPrice() {
+    public int getPrice() {
         return price;
     }
-    public boolean isOccupied() {
-        return isOccupied;
+    public void setAvailable(boolean available) {
+        this.available = available;
     }
-    public void setOccupied(boolean isOccupied) {
-        this.isOccupied = isOccupied;
+    public boolean isAvailable() {return available;}
+    public void addResident(Customer customer) {
+        residents.add(customer);
+    }
+    public ArrayList<Customer> getResidents() {
+        return residents;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public void addService(int serviceID) {
+        services.add(serviceID);
+    }
+    public ArrayList<Integer> getService() {
+        return services;
+    }
+    public Date getEntryDate() {
+        return entryDate;
+    }
+    public void setEntryDate(Date entryDate) {
+        this.entryDate = entryDate;
+    }
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
     }
 }
