5307682b873f4b04f15decd9c250e05ed4b88a6e
youssefgindy <youssefgindy7@gmail.com>
2025-12-11 19:18:55 +0200

    Update reservation.java

---
+++ reservation.java
@@ -0,0 +1,83 @@
+import java.util.Date;
+import java.util.List;
+
+public class Reservation {
+
+    private int ID;
+    private int customerID;
+    private int roomID;
+    private List<Integer> services;
+    private Date startDate;
+    private Date endDate;
+    private int price;
+
+    
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public int getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(int roomID) {
+        this.roomID = roomID;
+    }
+
+    public List<Integer> getServices() {
+        return services;
+    }
+
+    public void setServices(List<Integer> services) {
+        this.services = services;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    
+
+    public void addService(int serviceID) {
+    }
+
+    public void addServices(List<Integer> serviceIDs) {
+    }
+
+    public int getClient() {
+        return 0;
+    }
+}
