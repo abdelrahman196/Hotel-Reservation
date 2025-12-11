bec17dea9a6cb766af0830082503681476209f51
youssefgindy <youssefgindy7@gmail.com>
2025-12-11 19:00:36 +0200

    Create otherServices.java

---
+++ otherServices.java
@@ -0,0 +1,39 @@
+
+import java.util.ArrayList;
+
+public class OtherService {
+
+    
+    private ArrayList<Service> services;
+    private int currentID;
+
+    
+    public OtherService() {
+    }
+
+    
+
+    public void addService(String name, String description, int price, boolean available) {
+    }
+
+    public void updateService(int id, String name, String description, int price, boolean available) {
+    }
+
+    public void deleteService(int id) {
+    }
+
+    public Service getService(int serviceID) {
+        return null;
+    }
+
+    public ArrayList<Service> getAvailableServices() {
+        return null;
+    }
+
+    public void addServiceToClient(int clientID, int serviceID) {
+    }
+
+    public String getStaticReportServiceID(int serviceID) {
+        return null;
+    }
+}
