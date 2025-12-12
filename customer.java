77ecea4a9e992df08808fc43f052426081f8ea4e
sofalotfy <lotfysofa@gmail.com>
2025-12-12 15:14:51 +0200

    s

--- customer.java
+++ customer.java
@@ -4,13 +4,11 @@ public class customer {
     protected int id;
     protected String name;
     protected String phone;
-    private ArrayList<Integer> history;
     private int currentRoom;
-    public customer(int id, String name, String phone, ArrayList<Integer> history, int currentRoom) {
+    public customer(int id, String name, String phone, int currentRoom) {
         this.id = id;
         this.name = name;
         this.phone = phone;
-        this.history = new ArrayList<>();
         this.currentRoom = currentRoom;
     }
     public int getId() {
@@ -34,16 +32,5 @@ public class customer {
     public void setCurrentRoom(int currentRoom) {
         this.currentRoom = currentRoom;
     }
-    public ArrayList<Integer> getHistory() {
-        return history;
-    }
-    public void addReservation(int reservation){
-        this.history.add(new Integer(reservation) );
-    }
-    public void addReservation(ArrayList<Integer> reservation){
-        ArrayList<String> mergedList = new ArrayList<>();
-        mergedList.addAll(this.history);
-        mergedList.addAll(reservation);
-        this.history=mergedList;
-    }
+
 }
