7463c5f8fd7b9d16c56b835253bf1b765cbb78e6
sofalotfy <lotfysofa@gmail.com>
2025-12-12 19:12:56 +0200

    s

--- app/src/app/OtherService.java
+++ app/src/app/OtherService.java
@@ -1,5 +1,5 @@
 package app;
-
+import java.io.*;
 import java.util.ArrayList;
 
 public class OtherService {
@@ -17,7 +17,8 @@ public class OtherService {
 
 
     public void addService(String name, String description, int price, boolean available) {
-
+        File service_file = new File("")
+        PrintWriter
     }
 
     public void updateService(int id, String name, String description, int price, boolean available) {
