2562fca1ee47b4abb505f8f71b8405774adb413b
HanyAshraf7 <hanyashraf.engineer@gmail.com>
2025-12-16 14:15:28 +0200

    ...GUI

--- Main.java
+++ Main.java
@@ -4,11 +4,4 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new GUIStarter());
     }
-    static class GUIStarter implements Runnable {
-        @Override
-        public void run() {
-            HotelGUI gui = new HotelGUI();
-            gui.setVisible(true);
-        }
-    }
 }
